0c7107f9ccd097e95f01c0985b472a14308e5b34
Jidan Maharjan <jidanmaharjan1@gmail.com>
2022-11-17 23:20:04 +0545

    switch to render

--- src/components/Apiurl.js
+++ src/components/Apiurl.js
@@ -1,9 +1,9 @@
 
 //Heroku url
-export const baseUrl = 'https://hamrobachat.herokuapp.com/api/v1'
+// export const baseUrl = 'https://hamrobachat.herokuapp.com/api/v1'
 
 //Render.com url
-// export const baseUrl = 'https://hamrobachat.onrender.com/api/v1'
+export const baseUrl = 'https://hamrobachat.onrender.com/api/v1'
 
 //Development Url
 // export const baseUrl = 'http://localhost:4000/api/v1'
