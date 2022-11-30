37e7616476570b077dd482e4b46904732d41f67d
Jidan Maharjan <jidanmaharjan1@gmail.com>
2022-11-30 22:20:09 +0545

    notification tweaks

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -9,7 +9,10 @@ import { GoPrimitiveDot } from "react-icons/go";
 import { useState } from "react";
 import { getCookie } from "../components/getCookie";
 import moment from "moment/moment";
-import { getAllNotifications, seeAllNotifications } from "../services/bachatApi";
+import {
+  getAllNotifications,
+  seeAllNotifications,
+} from "../services/bachatApi";
 
 const Notifications = () => {
   const [toggle, setToggle] = useState(false);
@@ -26,26 +29,23 @@ const Notifications = () => {
   const { notifications } = useSelector((state) => state.bachat);
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    if(!isLoading){
-      if(!getCookie('token')){
-        navigate('/')
-        
+  useEffect(() => {
+    if (!isLoading) {
+      if (!getCookie("token")) {
+        navigate("/");
       }
-  }
-  },[])
+    }
+  }, []);
 
   const expandNoti = (id) => {
-    
-    let selected = document.getElementById("noti"+id)
+    let selected = document.getElementById("noti" + id);
     selected.classList.toggle("h-0");
-    
   };
 
-  const readAll =() =>{
-    dispatch(seeAllNotifications())
-    dispatch(getAllNotifications())
-  }
+  const readAll = () => {
+    dispatch(seeAllNotifications());
+    dispatch(getAllNotifications());
+  };
 
   if (isLoading) {
     return <Loader />;
@@ -54,9 +54,14 @@ const Notifications = () => {
     <div className=" p-4 mt-4">
       <div className="flex justify-between mb-4">
         <h1 className="my-2 font-bold text-gray-600">NOTIFICATIONS</h1>
-        <button className="py-0 px-2 bg-blue-400 hover:bg-blue-500 text-white rounded-md" onClick={()=>readAll()}>Mark all as read</button>
+        <button
+          className="py-0 px-2 bg-blue-400 hover:bg-blue-500 text-white rounded-md"
+          onClick={() => readAll()}
+        >
+          Mark all as read
+        </button>
       </div>
-      
+
       <div className="p-4 rounded-md bg-gray-100">
         <div>
           {notifications &&
@@ -66,7 +71,16 @@ const Notifications = () => {
                   className="flex items-center p-2  hover:bg-gray-200 cursor-pointer rounded-md"
                   onClick={() => expandNoti(index)}
                 >
-                  <GoPrimitiveDot className={`${notification.seen.find(one=>one._id.toString() === (profile && profile.user._id))? 'text-gray-400':'text-blue-400'}` }/>
+                  <GoPrimitiveDot
+                    className={`${
+                      notification.seen.find(
+                        (one) =>
+                          one._id.toString() === (profile && profile.user._id)
+                      )
+                        ? "text-gray-400"
+                        : "text-blue-400"
+                    }`}
+                  />
                   <div className="w-full ml-2 ">
                     <p className="">{notification.title}</p>
                     <p className="text-xs 2xl:text-sm text-gray-500">
@@ -75,13 +89,17 @@ const Notifications = () => {
                   </div>
                 </div>
                 <div
-                  id={'noti'+index}
+                  id={"noti" + index}
                   className={`h-0 overflow-hidden my-2 pl-8 text-gray-700`}
-                >{notification.content}
+                >
+                  {notification.content}
                 </div>
                 <div className="h-[1px] bg-gray-200 w-full mb-2 "></div>
               </div>
             ))}
+          {notifications && !notifications.length > 0 && (
+            <p className="font-semibold text-gray-500"> No Notifications </p>
+          )}
         </div>
       </div>
     </div>
